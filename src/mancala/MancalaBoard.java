a3a722b512006b3b273fbfe6b6d5fc60bd059059
nikkiihuynh <nikkijhuynh@gmail.com>
2024-11-23 02:19:15 -0800

    added parts of undo function, not complete

--- src/mancala/MancalaBoard.java
+++ src/mancala/MancalaBoard.java
@@ -1,5 +1,8 @@
 package mancala;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class MancalaBoard {
     private int[] pitsA; //pits for player A
     private int[] pitsB; //pits for player B
@@ -18,4 +21,48 @@ public class MancalaBoard {
     }
 
     // will add more methods in the future to handle the game logic (moving /capturing stones)
+
+    /**
+     * Nikki Huynh 
+     * Attempt for undo function using ArrayList and Stack.
+     */
+    private final ArrayList<Integer> board;
+    private final Stack<ArrayList<Integer>> undoFunction; 
+    private final int maxUndo = 3;
+    private int selectedUndos; // how many times the player has already used the undo function
+    
+    // set up the board; will update once we have finalized the board setup; using ArrayList might be ideal
+
+    public ArrayList<Integer> currentBoardState() {
+        return new ArrayList<>(board); // gets current state of the board
+    }
+
+    public void saveCurrentBoard() {
+        undoStack.push(new ArrayList<>(board)); // save the current state of the baord
+        selectedUndos = 0; //user has not selected to undo yet
+    }
+
+    public boolean undo() {
+        if(!undoFunction.isEmpty() && undoCount < maxUndo) {
+            ArrayList<Integer> previousBoardState = undoStack.pop();
+            for(int i = 0; i < board.size(); i++) {
+                board.set(i, previousBoardState.get(i));
+            }
+
+            selectedUndos++; // user has selected undo
+            return true; // user was able to undo (did not reach max)
+        }
+
+        return false; // user was not able to undo (met max)
+    }
+
+    public int getSelectedUndos() { // get the count of times user has selected undo
+        return selectedUndos;
+    }
+
+    public void resetSelectedUndos() { // reset the count of times user has selected undo per new turn
+        selectedUndos = 0; 
+    }
+
+    // still need to work on updating board after user has selected undo ... 
 }
